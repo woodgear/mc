69a4a3240b6b8c35de8a3a73c421fc51d15a6505
cong <q1875486458@gmail.com>
2023-10-13 16:13:04 +0800

    use zsh as default shell

--- lua/mc/config/actions/terminal.lua
+++ lua/mc/config/actions/terminal.lua
@@ -1,8 +1,9 @@
 local log = require("mc.util.vlog")
 
+vim.api.nvim_command(":set shell=/bin/zsh")
+
 local jump_to_terminal = function()
   vim.api.nvim_command(":ToggleTerm")
 end
-
 vim.keymap.set('n', '<A-m>',jump_to_terminal)
 vim.keymap.set('t', '<A-m>',jump_to_terminal)
