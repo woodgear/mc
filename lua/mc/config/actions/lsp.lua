0d5276041e77065a8a835fd3bc1c4007f5ab8c24
cong <q1875486458@gmail.com>
2023-11-28 14:57:50 +0800

    move lsp actions to actions/

---
+++ lua/mc/config/actions/lsp.lua
@@ -0,0 +1,30 @@
+local M= {}
+
+function M.on_attach(client,bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  local bufopts = {
+    noremap = true,
+    silent = true,
+    buffer = bufnr
+  }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', '<leader>jd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function()
+    vim.lsp.buf.format {
+      async = true
+    }
+  end, bufopts)
+end
+return M
--- lua/mc/config/lsp/lua.lua
+++
@@ -1,1 +0,0 @@
-local log = require "mc.util.vlog"
